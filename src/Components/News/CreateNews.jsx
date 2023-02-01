eb4612c5f650bcee52631e4386b305124e5e0053
unknown <zubair.rahman89@gmail.com>
2023-02-01 18:16:02 +0600

    AddNews: all field added and data added

--- src/Components/News/CreateNews.jsx
+++ src/Components/News/CreateNews.jsx
@@ -1,8 +1,11 @@
-import { Box, TextField, Typography } from "@mui/material";
-import React from "react";
+import { Box, MenuItem, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { categories, newsType, status } from "../../data/AddNewsData";
 import TextEditor from "../Shared/TextEditor";
 
 const CreateNews = () => {
+  const [value, setValue] = useState("");
+
   return (
     <Box sx={{ width: "930px" }}>
       <Box>
@@ -22,7 +25,125 @@ const CreateNews = () => {
           }}
         />
       </Box>
-      <TextEditor />
+      <TextEditor setValue={setValue} />
+      {/* 1st selection section author, category, file */}
+      <Box
+        component="form"
+        sx={{ display: "flex", justifyContent: "space-between", my: "30px" }}
+      >
+        {/* author */}
+        <Box>
+          <Typography variant="body4" color="textBlack">
+            Author Name
+          </Typography>
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            placeholder="Type here... "
+            sx={{
+              border: 1,
+              borderColor: "primary.main",
+              width: "295px",
+              borderRadius: "5px",
+              mt: "10px",
+            }}
+          />
+        </Box>
+        {/* categories */}
+        <Box>
+          <Typography variant="body4" color="textBlack">
+            Categories
+          </Typography>
+          <TextField
+            id="outlined-select"
+            select
+            defaultValue="Bangladesh"
+            sx={{
+              border: 1,
+              borderColor: "primary.main",
+              width: "295px",
+              borderRadius: "5px",
+              mt: "10px",
+            }}
+          >
+            {categories.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+        {/* file */}
+        <Box>
+          <Typography variant="body4" color="textBlack">
+            Image
+          </Typography>
+          <TextField
+            id="outlined-select"
+            type="file"
+            sx={{
+              border: 1,
+              borderColor: "primary.main",
+              width: "295px",
+              borderRadius: "5px",
+              mt: "10px",
+            }}
+          ></TextField>
+        </Box>
+      </Box>
+      {/* 1st selection end */}
+      {/* 2nd selection start */}
+      <Box sx={{ display: "flex", gap: "24px" }}>
+        {/* news Type */}
+        <Box>
+          <Typography variant="body4" color="textBlack">
+            News Type
+          </Typography>
+          <TextField
+            id="outlined-select"
+            select
+            defaultValue="Top News"
+            sx={{
+              border: 1,
+              borderColor: "primary.main",
+              width: "295px",
+              borderRadius: "5px",
+              mt: "10px",
+            }}
+          >
+            {newsType.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+        {/* status */}
+        <Box>
+          <Typography variant="body4" color="textBlack">
+            Status
+          </Typography>
+          <TextField
+            id="outlined-select"
+            select
+            defaultValue="Draft"
+            sx={{
+              border: 1,
+              borderColor: "primary.main",
+              width: "295px",
+              borderRadius: "5px",
+              mt: "10px",
+            }}
+          >
+            {status.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+      </Box>
+      {/* 2nd selection end */}
     </Box>
   );
 };
--- src/Components/Shared/TextEditor.jsx
+++ src/Components/Shared/TextEditor.jsx
@@ -1,59 +1,39 @@
-import React from "react";
-import { $getRoot, $getSelection } from "lexical";
-import { useEffect } from "react";
+import React, { useMemo, useRef } from "react";
+import JoditEditor, { Jodit } from "jodit-react";
+import { Box, Typography } from "@mui/material";
 
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-
-const theme = {
-  backgroundColor: "red",
-};
-
-function onChange(editorState) {
-  editorState.read(() => {
-    const root = $getRoot();
-    const selection = $getSelection();
-
-    console.log(root, selection);
-  });
-}
-
-function MyCustomAutoFocusPlugin() {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    editor.focus();
-  }, [editor]);
-
-  return null;
-}
-
-function onError(error) {
-  console.error(error);
-}
-
-const TextEditor = () => {
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme,
-    onError,
+const TextEditor = ({ setValue }) => {
+  const editor = useRef(null);
+  const config = {
+    buttons: ["align", "bold", "italic", "underline"],
+    placeholder: "",
   };
+
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>Enter some text...</div>}
-        ErrorBoundary={LexicalErrorBoundary}
+    <Box
+      sx={{
+        "& .jodit-status-bar .jodit-status-bar__item a ": {
+          display: "none",
+        },
+        "& .jodit-container:not(.jodit_inline)": {
+          borderColor: "primary.main",
+        },
+        "& .jodit-add-new-line span": {
+          display: "none",
+        },
+        mt: "30px",
+      }}
+    >
+      <Typography variant="body4" color="textBlack" sx={{ mb: "10px" }}>
+        Discription
+      </Typography>
+      <JoditEditor
+        ref={editor}
+        onBlur={(newContent) => setValue(newContent)}
+        config={config}
       />
-      <OnChangePlugin onChange={onChange} />
-      <HistoryPlugin />
-      <MyCustomAutoFocusPlugin />
-    </LexicalComposer>
+    </Box>
   );
 };
+
 export default TextEditor;
