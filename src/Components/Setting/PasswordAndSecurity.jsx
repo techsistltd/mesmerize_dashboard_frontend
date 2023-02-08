a10fc84e3cf965b78f37b5765887e7fe1a6b8872
unknown <zubair.rahman89@gmail.com>
2023-02-08 16:01:20 +0600

    Setting: page complete

--- src/Components/Setting/PasswordAndSecurity.jsx
+++ src/Components/Setting/PasswordAndSecurity.jsx
@@ -2,17 +2,21 @@ import {
   Box,
   Button,
   Checkbox,
+  FormControlLabel,
   Grid,
+  Radio,
+  RadioGroup,
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { Fragment, useState } from "react";
 import TitleUnderLine from "./TitleUnderLine";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
 import { AiFillEyeInvisible } from "react-icons/ai";
 
 const PasswordAndSecurity = () => {
+  const [selectedType, setSelectedType] = useState("change");
   return (
     <Box>
       <TitleUnderLine title="Password & Security" underline />
@@ -20,112 +24,137 @@ const PasswordAndSecurity = () => {
         All password settings and security option.
       </Typography>
       {/* radio button */}
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: "20px",
-          my: "30px",
-        }}
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+        value={selectedType}
+        onChange={(e) => setSelectedType(e.target.value)}
+        sx={{ my: "30px" }}
       >
-        {/* button 1 */}
-        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
-          <Checkbox
-            sx={{ padding: 0 }}
-            icon={<RadioButtonUncheckedIcon />}
-            checkedIcon={
-              <RadioButtonCheckedIcon sx={{ color: "color15.main" }} />
-            }
-          />
-          <Typography>Change Password</Typography>
-        </Box>
-        {/* button 1 end */}
-        {/* button 2 */}
-        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
-          <Checkbox
-            sx={{ padding: 0 }}
-            icon={<RadioButtonUncheckedIcon />}
-            checkedIcon={
-              <RadioButtonCheckedIcon sx={{ color: "color15.main" }} />
-            }
-          />
-          <Typography>Forgot Password</Typography>
-        </Box>
-        {/* button 2 end */}
-      </Box>
+        <FormControlLabel
+          value="change"
+          control={<Radio color="color15" />}
+          label="Change Password"
+        />
+        <FormControlLabel
+          value="forget"
+          control={<Radio color="color15" />}
+          label="Forgot Password"
+        />
+      </RadioGroup>
+
       {/* radio button end */}
       <Grid container columnSpacing={"25px"}>
-        <Grid item xs={3.5}>
-          <Typography variant="body4" color="textBlack">
-            Old password
-          </Typography>
-          <TextField
-            fullWidth
-            type={"password"}
-            id="outlined-basic"
-            variant="outlined"
-            placeholder="**************"
-            sx={{
-              border: 1,
-              borderColor: "primary.main",
-              height: "40px",
-              borderRadius: "5px",
-              mt: "10px",
-              "& .MuiInputBase-input": {
-                padding: "8px",
-              },
-            }}
-          />
-        </Grid>
-        <Grid item xs={3.5}>
-          <Typography variant="body4" color="textBlack">
-            New password
-          </Typography>
-          <TextField
-            fullWidth
-            type={"password"}
-            id="outlined-basic"
-            variant="outlined"
-            placeholder="**************"
-            sx={{
-              border: 1,
-              borderColor: "primary.main",
-              height: "40px",
-              borderRadius: "5px",
-              mt: "10px",
-              "& .MuiInputBase-input": {
-                padding: "8px",
-              },
-            }}
-          />
-        </Grid>
-        <Grid item xs={3.5}>
-          <Typography variant="body4" color="textBlack">
-            Confirm New password
-          </Typography>
-          <TextField
-            fullWidth
-            type={"password"}
-            id="outlined-basic"
-            variant="outlined"
-            placeholder="**************"
-            sx={{
-              border: 1,
-              borderColor: "primary.main",
-              height: "40px",
-              borderRadius: "5px",
-              mt: "10px",
-              "& .MuiInputBase-input": {
-                padding: "8px",
-              },
-            }}
-          />
-        </Grid>
-        <Grid item xs={1.5} sx={{ display: "flex", alignItems: "end" }}>
-          <Button variant="button3" sx={{ height: "40px" }}>
-            Submit
-          </Button>
-        </Grid>
+        {Boolean(selectedType === "change") ? (
+          <Fragment>
+            {/* change password  */}
+            <Grid item xs={3.5}>
+              <Typography variant="body4" color="textBlack">
+                Old password
+              </Typography>
+              <TextField
+                fullWidth
+                type={"password"}
+                id="outlined-basic"
+                variant="outlined"
+                placeholder="**************"
+                sx={{
+                  border: 1,
+                  borderColor: "primary.main",
+                  height: "40px",
+                  borderRadius: "5px",
+                  mt: "10px",
+                  "& .MuiInputBase-input": {
+                    padding: "8px",
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={3.5}>
+              <Typography variant="body4" color="textBlack">
+                New password
+              </Typography>
+              <TextField
+                fullWidth
+                type={"password"}
+                id="outlined-basic"
+                variant="outlined"
+                placeholder="**************"
+                sx={{
+                  border: 1,
+                  borderColor: "primary.main",
+                  height: "40px",
+                  borderRadius: "5px",
+                  mt: "10px",
+                  "& .MuiInputBase-input": {
+                    padding: "8px",
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={3.5}>
+              <Typography variant="body4" color="textBlack">
+                Confirm New password
+              </Typography>
+              <TextField
+                fullWidth
+                type={"password"}
+                id="outlined-basic"
+                variant="outlined"
+                placeholder="**************"
+                sx={{
+                  border: 1,
+                  borderColor: "primary.main",
+                  height: "40px",
+                  borderRadius: "5px",
+                  mt: "10px",
+                  "& .MuiInputBase-input": {
+                    padding: "8px",
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={1.5} sx={{ display: "flex", alignItems: "end" }}>
+              <Button variant="button3" sx={{ height: "40px" }}>
+                Submit
+              </Button>
+            </Grid>
+            {/* change password end */}
+          </Fragment>
+        ) : (
+          <Fragment>
+            {/* forget password */}
+            <Grid item xs={5}>
+              <Typography variant="body4" color="textBlack">
+                Please Enter Your Registered Email
+              </Typography>
+              <TextField
+                fullWidth
+                type={"text"}
+                id="outlined-basic"
+                variant="outlined"
+                placeholder="Enter Your Mail "
+                sx={{
+                  border: 1,
+                  borderColor: "primary.main",
+                  height: "40px",
+                  borderRadius: "5px",
+                  mt: "10px",
+                  "& .MuiInputBase-input": {
+                    padding: "8px",
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={1.5} sx={{ display: "flex", alignItems: "end" }}>
+              <Button variant="button3" sx={{ height: "40px" }}>
+                Send
+              </Button>
+            </Grid>
+            {/* forget password end */}
+          </Fragment>
+        )}
       </Grid>
     </Box>
   );
