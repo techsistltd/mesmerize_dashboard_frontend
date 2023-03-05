d2d423113416d33c642145f5706134a3d8e150a3
ZèD <imzihad@gmail.com>
2023-03-05 11:20:03 +0600

    axios: switched to remote dev server
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Utils/axiosApi.js
+++ src/Utils/axiosApi.js
@@ -9,8 +9,8 @@ import {
 const onProd = Boolean(process.env.NODE_ENV === "production");
 
 const baseURL = onProd
-  ? "https://mesmerize-dev.techsistltd.com"
-  : "http://192.168.1.100:8000";
+  ? "https://mesmerize-apis.techsistltd.com"
+  : "https://mesmerize-dev.techsistltd.com";
 
 // const baseURL = "https://mesmerize-apis.techsistltd.com";
 
